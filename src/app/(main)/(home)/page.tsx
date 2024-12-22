26544f53f488697ef68d302109127fc192315661
remus-wolico <135813551+remus00@users.noreply.github.com>
2024-12-22 20:17:00 +0100

    trending topics sidebar

--- src/app/(main)/(home)/page.tsx
+++ src/app/(main)/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { TrendsSidebar } from '@/components/custom/trends-sidebar';
 import { PostEditor } from '@/components/posts/editor/post-editor';
 import { Post } from '@/components/posts/post';
 import prisma from '@/lib/prisma';
@@ -9,13 +10,14 @@ export default async function Home() {
         orderBy: { createdAt: 'desc' },
     });
     return (
-        <div className="w-full min-w-0">
+        <div className="flex w-full min-w-0 gap-5">
             <div className="w-full min-w-0 space-y-5">
                 <PostEditor />
                 {posts.map((post) => (
                     <Post key={post.id} post={post} />
                 ))}
             </div>
+            <TrendsSidebar />
         </div>
     );
 }
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -18,3 +18,10 @@ export function formatRelativeDate(from: Date) {
         }
     }
 }
+
+export function formatNumber(num: number) {
+    return Intl.NumberFormat('en-US', {
+        notation: 'compact',
+        maximumFractionDigits: 1,
+    }).format(num);
+}
